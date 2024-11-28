2f2e793e60f516e13f1b2f88afb2a911de01c4e2
wooB0328 <tptmzh2012@gmail.com>
2024-11-28 17:09:46 +0900

    first nodeJS update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Node JS Server is  running at http://${hostname}:${port}/`);
+  console.log(`재밌는 오픈소스소프트웨어 http://${hostname}:${port}/`);
 });
 
 module.exports = server;
