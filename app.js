969a653ffd012e8ded840cd787276af69f6e8b9a
wooB0328 <tptmzh2012@gmail.com>
2024-11-28 17:24:07 +0900

    second update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.end('재밌는 오픈소스소프트웨어\n');
 });
 
 server.listen(port, hostname, () => {
